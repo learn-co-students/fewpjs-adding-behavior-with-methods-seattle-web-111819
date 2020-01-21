3c5f05ea6f6f8ddd138928e2ada777f846ce8e29
Alice Won <alicewon4@gmail.com>
2020-01-20 19:30:05 -0800

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Your code here
+class Cat {
+  constructor(name, sex) {
+    this.name = name
+    this.sex = sex
+  }
+
+  speak() {
+    return `${this.name} says meow!`
+  }
+}
+
+class Dog {
+  constructor(name, sex) {
+    this.name = name
+    this.sex = sex
+  }
+
+  speak() {
+    return `${this.name} says woof!`
+  }
+}
+
+class Bird {
+  constructor(name, sex) {
+    this.name = name
+    this.sex = sex
+  }
+
+  speak() {
+    return `${this.name} says squak!`
+  }
+}
